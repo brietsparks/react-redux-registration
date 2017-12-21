753220977edb65e32efdc069a0f11ee843e68850
Briet Sparks <bws46@cornell.edu>
2017-12-21 01:05:45 -0600

    feat(example): Created exmple

--- src/react-redux-registration.ts
+++ src/react-redux-registration.ts
@@ -1,4 +1,4 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+export { default as fieldCheckReducer } from './reducers/fieldCheckReducer'
+export { default as fieldAvailabilityReducer } from './reducers/fieldAvailabilityReducer'
+
+export { default as checkUserExistsByEmail } from './middleware/checkUserExistsByEmail'
