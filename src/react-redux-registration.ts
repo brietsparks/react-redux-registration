065ebf590d592073a00aae552a2ce16e27d67f46
Briet Sparks <bws46@cornell.edu>
2017-12-22 16:07:34 -0600

    feat(main): Add exports

--- src/react-redux-registration.ts
+++ src/react-redux-registration.ts
@@ -2,3 +2,7 @@ export { default as fieldCheckReducer } from './reducers/fieldCheckReducer'
 export { default as fieldAvailabilityReducer } from './reducers/fieldAvailabilityReducer'
 
 export { default as checkUserExistsByEmail } from './middleware/checkUserExistsByEmail'
+export { default as checkEmailExists } from './middleware/checkEmailExists'
+export { default as sendVerificationCode } from './middleware/sendVerificationCode'
+export { default as checkVerificationCode } from './middleware/checkVerificationCode'
+export { default as checkUserExistsByUsername } from './middleware/checkUserExistsByUsername'
