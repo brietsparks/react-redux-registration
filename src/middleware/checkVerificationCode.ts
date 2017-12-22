b316868fbb9a28da99dc055162ae84b72669f60d
Briet Sparks <bws46@cornell.edu>
2017-12-21 21:24:00 -0600

    feat(checkVerificationCode): Created checkVerificationCode mw and tests

---
+++ src/middleware/checkVerificationCode.ts
@@ -0,0 +1,36 @@
+import { Middleware } from '../types'
+import { CHECK_VERIFICATION_CODE } from '../actionTypes'
+import {
+  verificationCodeMatches,
+  verificationCodeMismatch,
+  registrationCheckError
+} from '../actionCreators'
+
+export const checkVerificationCode = (
+  checkVerificationCode: (email: string) => Promise<Boolean>
+): Middleware => {
+  return (store: any) => (next: any) => (action: any) => {
+    const { dispatch } = store
+    const email = action.payload
+
+    if (action.type === CHECK_VERIFICATION_CODE) {
+      checkVerificationCode(email)
+        .then(result => {
+          if (result === true) {
+            dispatch(verificationCodeMatches())
+          }
+
+          if (result === false) {
+            dispatch(verificationCodeMismatch())
+          }
+        })
+        .catch(error => {
+          dispatch(registrationCheckError(CHECK_VERIFICATION_CODE, error))
+        })
+    }
+
+    next(action)
+  }
+}
+
+export default checkVerificationCode
---
+++ test/middleware/checkVerificationCode.test.ts
@@ -0,0 +1,60 @@
+import checkVerificationCode from '../../src/middleware/checkVerificationCode'
+import {
+  CHECK_VERIFICATION_CODE,
+  VERIFICATION_CODE_MATCHES,
+  VERIFICATION_CODE_MISMATCH,
+  REGISTRATION_CHECK_ERROR
+} from '../../src/actionTypes'
+import { flushPromises } from '../util'
+
+describe('checkEmailExists', () => {
+  let store
+  let next
+
+  beforeEach(() => {
+    store = { dispatch: jest.fn() }
+    next = jest.fn()
+  })
+
+  describe(`intercepting ${CHECK_VERIFICATION_CODE}`, () => {
+    it('calls the user-provided function', () => {
+      const check = jest.fn(email => Promise.resolve(true))
+      const middleware = checkVerificationCode(check)
+      middleware(store)(next)({ type: CHECK_VERIFICATION_CODE, payload: 'a@a.a' })
+      expect(check.mock.calls.length).toBe(1)
+    })
+
+    it(`dispatches ${VERIFICATION_CODE_MATCHES} if the user-provided function resolves to true`, async () => {
+      const check = jest.fn(email => Promise.resolve(true))
+      const middleware = checkVerificationCode(check)
+      middleware(store)(next)({ type: CHECK_VERIFICATION_CODE, payload: 'a@a.a' })
+
+      await flushPromises()
+      expect(store.dispatch).toBeCalledWith({ type: VERIFICATION_CODE_MATCHES })
+    })
+
+    it(`dispatches ${VERIFICATION_CODE_MISMATCH} if the user-provided function resolves to false`, async () => {
+      const check = jest.fn(email => Promise.resolve(false))
+      const middleware = checkVerificationCode(check)
+      middleware(store)(next)({ type: CHECK_VERIFICATION_CODE, payload: 'a@a.a' })
+
+      await flushPromises()
+      expect(store.dispatch).toBeCalledWith({ type: VERIFICATION_CODE_MISMATCH })
+    })
+
+    it(`dispatches ${REGISTRATION_CHECK_ERROR} if the user-provided function throws error`, async () => {
+      const check = jest.fn(email => Promise.reject({ rejectionMessage: 'some message' }))
+      const middleware = checkVerificationCode(check)
+      middleware(store)(next)({ type: CHECK_VERIFICATION_CODE, payload: 'a@a.a' })
+
+      await flushPromises()
+      expect(store.dispatch).toBeCalledWith({
+        type: REGISTRATION_CHECK_ERROR,
+        payload: {
+          onType: CHECK_VERIFICATION_CODE,
+          error: { rejectionMessage: 'some message' }
+        }
+      })
+    })
+  })
+})
