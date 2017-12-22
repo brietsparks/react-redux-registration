5df235abc61ef13912be082da429ae956ad77bfd
Briet Sparks <bws46@cornell.edu>
2017-12-22 16:09:16 -0600

    feat(selectors): Selectors wip

--- src/Selectors.ts
+++ src/Selectors.ts
@@ -1,15 +1,100 @@
-import { SelectorsArgs, RegistrationState, FieldAvailabilityState } from './types'
+import {
+  SelectorsArgs,
+  RegistrationSelector,
+  RegistrationState,
+  FieldAvailabilityState
+} from './types'
+import * as steps from './stepNames'
+import { defaultState } from './reducers/fieldAvailabilityReducer'
 
-export class Selectors {
-  getSubstate: (state: any) => RegistrationState
+export const getFieldAvailability = (r: RegistrationSelector) => (state: any) => {
+  return r(state).fieldAvailability
+}
 
-  constructor(args: SelectorsArgs) {
-    this.getSubstate = args.registrationSelector
+export const getStep = (r: RegistrationSelector) => (state: any) => {
+  const current = getFieldAvailability(r)(state)
+
+  if (
+    stateMatches(current, {
+      userExistsByEmail: undefined,
+      emailExists: undefined,
+      verificationCodeSendOk: undefined,
+      verificationCodeMatches: undefined,
+      userExistsByUsername: undefined
+    })
+  ) {
+    return steps.PROVIDE_EMAIL
   }
 
-  getFieldAvailability(state: any) {
-    return this.getSubstate(state).fieldAvailability
+  if (
+    stateMatches(current, {
+      userExistsByEmail: true,
+      emailExists: undefined,
+      verificationCodeSendOk: undefined,
+      verificationCodeMatches: undefined,
+      userExistsByUsername: undefined
+    })
+  ) {
+    return steps.USER_EXISTS_BY_EMAIL
+  }
+
+  if (
+    stateMatches(current, {
+      userExistsByEmail: false,
+      emailExists: true,
+      verificationCodeSendOk: true,
+      verificationCodeMatches: undefined,
+      userExistsByUsername: undefined
+    })
+  ) {
+    return steps.PROVIDE_VERIFICATION_CODE
+  }
+
+  if (
+    stateMatches(current, {
+      userExistsByEmail: false,
+      emailExists: true,
+      verificationCodeSendOk: false,
+      verificationCodeMatches: undefined,
+      userExistsByUsername: undefined
+    })
+  ) {
+    return steps.VERIFICATION_CODE_SEND_ERROR
+  }
+
+  if (
+    stateMatches(current, {
+      userExistsByEmail: false,
+      emailExists: true,
+      verificationCodeSendOk: true,
+      verificationCodeMatches: true,
+      userExistsByUsername: undefined
+    }) ||
+    stateMatches(current, {
+      userExistsByEmail: false,
+      emailExists: false,
+      verificationCodeSendOk: undefined,
+      verificationCodeMatches: undefined,
+      userExistsByUsername: undefined
+    })
+  ) {
+    return steps.PROVIDE_USERNAME
   }
 }
 
-export default Selectors
+function stateMatches(currentState: FieldAvailabilityState, evalState: FieldAvailabilityState) {
+  return (
+    currentState.userExistsByEmail === evalState.userExistsByEmail &&
+    currentState.emailExists === evalState.emailExists &&
+    currentState.verificationCodeSendOk === evalState.verificationCodeSendOk &&
+    currentState.userExistsByEmail === evalState.userExistsByEmail
+  )
+}
+
+export default (args: SelectorsArgs) => {
+  const { registrationSelector } = args
+
+  return {
+    getFieldAvailability: getFieldAvailability(registrationSelector)
+  }
+}
