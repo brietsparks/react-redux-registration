a3dfb5814262d6b726abe7bc5f06c5915dca7604
Briet Sparks <bws46@cornell.edu>
2017-12-27 19:25:39 -0600

    refactor: Change step logic to allow for optional username

--- src/selectors.ts
+++ src/selectors.ts
@@ -2,18 +2,72 @@ import {
   SelectorsArgs,
   RegistrationSelector,
   RegistrationState,
-  FieldAvailabilityState
+  FieldAvailabilityState,
+  Options
 } from './types'
 import * as steps from './stepNames'
 import { defaultState } from './reducers/fieldAvailabilityReducer'
-import {
-  checkUserExistsByEmail,
-  checkVerificationCode,
-  checkUserExistsByUsername
-} from './actionCreators'
 
 export const defaultRegistrationSelector = (state: any): RegistrationState => {
   return state.registration
+}
+
+export const getFieldAvailability = (r: RegistrationSelector) => (
+  state: any
+): FieldAvailabilityState => {
+  return r(state).fieldAvailability
+}
+
+export const emailProvided = (r: RegistrationSelector) => (state: any): Boolean => {
+  const current = getFieldAvailability(r)(state)
+  return (
+    current.userExistsByEmail === false ||
+    (current.userExistsByEmail === true && current.emailExists === false)
+  )
+}
+
+export const verificationRequired = (r: RegistrationSelector) => (state: any): Boolean => {
+  return getFieldAvailability(r)(state).verificationCodeSendOk === true
+}
+
+export const verified = (r: RegistrationSelector) => (state: any): Boolean => {
+  return getFieldAvailability(r)(state).verificationCodeMatches === true
+}
+
+export const emailLockedIn = (r: RegistrationSelector) => (state: any): Boolean => {
+  const current = getFieldAvailability(r)(state)
+  return (
+    (current.userExistsByEmail === false && current.emailExists === false) || verified(r)(state)
+  )
+}
+
+export const usernameLockedIn = (r: RegistrationSelector) => (state: any): Boolean => {
+  return getFieldAvailability(r)(state).userExistsByUsername === false
+}
+
+const defaultOptions: Options = { withUsername: true }
+
+export const getStep = (
+  r: RegistrationSelector = defaultRegistrationSelector,
+  options: Options = defaultOptions
+) => (s: any): String => {
+  if (!emailProvided(r)(s)) {
+    return steps.PROVIDE_EMAIL
+  }
+
+  if (emailProvided(r)(s) && verificationRequired(r)(s) && !verified(r)(s)) {
+    return steps.PROVIDE_VERIFICATION_CODE
+  }
+
+  if (emailLockedIn(r)(s)) {
+    if (options.withUsername && !usernameLockedIn(r)(s)) {
+      return steps.PROVIDE_USERNAME
+    }
+
+    return steps.CHECKS_COMPLETE
+  }
+
+  return steps.UNDEFINED_STEP
 }
 
 export const fieldIsActive = (field: String, step: String): Boolean => {
@@ -32,103 +86,11 @@ export const fieldIsActive = (field: String, step: String): Boolean => {
   return false
 }
 
-export const getFieldAvailability = (r: RegistrationSelector) => (
-  state: any
-): FieldAvailabilityState => {
-  return r(state).fieldAvailability
+export default (
+  r: RegistrationSelector = defaultRegistrationSelector,
+  options: Options = defaultOptions
+) => {
+  return {
+    getStep: getStep(r, options)
+  }
 }
-
-export const getStep = (r: RegistrationSelector) => (state: any): String => {
-  const current = getFieldAvailability(r)(state)
-
-  if (
-    stateMatches(current, {
-      userExistsByEmail: undefined,
-      emailExists: undefined,
-      verificationCodeSendOk: undefined,
-      verificationCodeMatches: undefined,
-      userExistsByUsername: undefined
-    })
-  ) {
-    return steps.PROVIDE_EMAIL
-  }
-
-  if (
-    stateMatches(current, {
-      userExistsByEmail: true,
-      emailExists: undefined,
-      verificationCodeSendOk: undefined,
-      verificationCodeMatches: undefined,
-      userExistsByUsername: undefined
-    })
-  ) {
-    return steps.PROVIDE_EMAIL
-  }
-
-  if (
-    stateMatches(current, {
-      userExistsByEmail: false,
-      emailExists: true,
-      verificationCodeSendOk: true,
-      verificationCodeMatches: undefined,
-      userExistsByUsername: undefined
-    })
-  ) {
-    return steps.PROVIDE_VERIFICATION_CODE
-  }
-
-  if (
-    stateMatches(current, {
-      userExistsByEmail: false,
-      emailExists: true,
-      verificationCodeSendOk: false,
-      verificationCodeMatches: undefined,
-      userExistsByUsername: undefined
-    })
-  ) {
-    return steps.VERIFICATION_CODE_SEND_ERROR
-  }
-
-  if (
-    stateMatches(current, {
-      userExistsByEmail: false,
-      emailExists: true,
-      verificationCodeSendOk: true,
-      verificationCodeMatches: true,
-      userExistsByUsername: undefined
-    })
-  ) {
-    return steps.PROVIDE_USERNAME
-  }
-
-  if (
-    stateMatches(current, {
-      userExistsByEmail: false,
-      emailExists: false,
-      verificationCodeSendOk: undefined,
-      verificationCodeMatches: undefined,
-      userExistsByUsername: undefined
-    })
-  ) {
-    return steps.PROVIDE_USERNAME
-  }
-
-  return steps.UNDEFINED_STEP
-}
-
-function stateMatches(currentState: FieldAvailabilityState, evalState: FieldAvailabilityState) {
-  return (
-    currentState.userExistsByEmail === evalState.userExistsByEmail &&
-    currentState.emailExists === evalState.emailExists &&
-    currentState.verificationCodeSendOk === evalState.verificationCodeSendOk &&
-    currentState.verificationCodeMatches === evalState.verificationCodeMatches &&
-    currentState.userExistsByEmail === evalState.userExistsByEmail
-  )
-}
-
-// export default (args: SelectorsArgs) => {
-//   const { registrationSelector } = args
-//
-//   return {
-//     getFieldAvailability: getFieldAvailability(registrationSelector)
-//   }
--- test/selectors.test.ts
+++ test/selectors.test.ts
@@ -2,6 +2,7 @@ import {
   defaultRegistrationSelector,
   fieldIsActive,
   getFieldAvailability,
+  emailLockedIn,
   getStep,
   getActionCreator
 } from '../src/selectors'
@@ -18,6 +19,232 @@ let registrationSelector = state => state.registration
 describe('defaultRegistrationSelector', () => {
   it('it returns the registration attribute of global state', () => {
     expect(defaultRegistrationSelector({ registration: 0 })).toEqual(0)
+  })
+})
+
+describe('getFieldAvailability', () => {
+  it('selects fieldAvailability substate', () => {
+    expect(
+      getFieldAvailability(state => state.registration)({
+        registration: {
+          fieldAvailability: 0
+        }
+      })
+    ).toEqual(0)
+  })
+})
+
+describe('emailLockedIn', () => {
+  it('returns false for a particular set of finite states', () => {
+    expect(
+      emailLockedIn(registrationSelector)({
+        registration: {
+          fieldAvailability: {}
+        }
+      })
+    ).toEqual(false)
+
+    expect(
+      emailLockedIn(registrationSelector)({
+        registration: {
+          fieldAvailability: { userExistsByEmail: true }
+        }
+      })
+    ).toEqual(false)
+
+    expect(
+      emailLockedIn(registrationSelector)({
+        registration: {
+          fieldAvailability: {
+            userExistsByEmail: false,
+            emailExists: undefined
+          }
+        }
+      })
+    ).toEqual(false)
+
+    expect(
+      emailLockedIn(registrationSelector)({
+        registration: {
+          fieldAvailability: {
+            userExistsByEmail: false,
+            emailExists: true,
+            verificationCodeMatches: undefined
+          }
+        }
+      })
+    ).toEqual(false)
+
+    expect(
+      emailLockedIn(registrationSelector)({
+        registration: {
+          fieldAvailability: {
+            userExistsByEmail: false,
+            emailExists: true,
+            verificationCodeMatches: false
+          }
+        }
+      })
+    ).toEqual(false)
+  })
+
+  it('returns true for a particular set of finite states', () => {
+    expect(
+      emailLockedIn(registrationSelector)({
+        registration: {
+          fieldAvailability: {
+            userExistsByEmail: false,
+            emailExists: false
+          }
+        }
+      })
+    ).toEqual(true)
+
+    expect(
+      emailLockedIn(registrationSelector)({
+        registration: {
+          fieldAvailability: {
+            userExistsByEmail: false,
+            emailExists: true,
+            verificationCodeMatches: true
+          }
+        }
+      })
+    ).toEqual(true)
+  })
+})
+
+describe('getStep', () => {
+  it(`returns ${steps.PROVIDE_EMAIL} when userExistsByEmail is undefined`, () => {
+    state = {
+      registration: {
+        fieldAvailability: {
+          userExistsByEmail: undefined
+        }
+      }
+    }
+
+    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_EMAIL)
+  })
+
+  it(`returns ${steps.PROVIDE_EMAIL} when userExistsByEmail is true`, () => {
+    state = {
+      registration: {
+        fieldAvailability: {
+          userExistsByEmail: true
+        }
+      }
+    }
+
+    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_EMAIL)
+  })
+
+  it(`returns ${
+    steps.PROVIDE_VERIFICATION_CODE
+  } when the email was provided and the verification code is sent but not verified`, () => {
+    state = {
+      registration: {
+        fieldAvailability: {
+          userExistsByEmail: false,
+          emailExists: true,
+          verificationCodeSendOk: true,
+          verificationCodeMatches: undefined
+        }
+      }
+    }
+
+    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_VERIFICATION_CODE)
+
+    state = {
+      registration: {
+        fieldAvailability: {
+          userExistsByEmail: false,
+          emailExists: true,
+          verificationCodeSendOk: true,
+          verificationCodeMatches: undefined
+        }
+      }
+    }
+
+    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_VERIFICATION_CODE)
+  })
+
+  // it(`evaluates state for step ${steps.VERIFICATION_CODE_SEND_ERROR}`, () => {
+  //   state = {
+  //     registration: {
+  //       fieldAvailability: {
+  //         userExistsByEmail: false,
+  //         emailExists: true,
+  //         verificationCodeSendOk: false,
+  //         verificationCodeMatches: undefined,
+  //         userExistsByUsername: undefined
+  //       }
+  //     }
+  //   }
+  //
+  //   expect(getStep(registrationSelector)(state)).toEqual(steps.VERIFICATION_CODE_SEND_ERROR)
+  // })
+
+  describe('when email is locked in', () => {
+    const emailLockedInState = {
+      userExistsByEmail: false,
+      emailExists: false,
+      verificationCodeSendOk: undefined,
+      verificationCodeMatches: undefined
+    }
+
+    it(`returns ${steps.PROVIDE_USERNAME} when username is undefined`, () => {
+      state = {
+        registration: {
+          fieldAvailability: {
+            ...emailLockedInState,
+            userExistsByUsername: undefined
+          }
+        }
+      }
+
+      expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_USERNAME)
+    })
+
+    it(`returns ${steps.PROVIDE_USERNAME} when username exists`, () => {
+      state = {
+        registration: {
+          fieldAvailability: {
+            ...emailLockedInState,
+            userExistsByUsername: true
+          }
+        }
+      }
+
+      expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_USERNAME)
+    })
+
+    it(`returns ${steps.CHECKS_COMPLETE} when username DNE`, () => {
+      state = {
+        registration: {
+          fieldAvailability: {
+            ...emailLockedInState,
+            userExistsByUsername: false
+          }
+        }
+      }
+
+      expect(getStep(registrationSelector)(state)).toEqual(steps.CHECKS_COMPLETE)
+    })
+
+    it(`returns ${steps.CHECKS_COMPLETE} when username check is disabled`, () => {
+      state = {
+        registration: {
+          fieldAvailability: {
+            ...emailLockedInState
+          }
+        }
+      }
+
+      expect(getStep(registrationSelector, { withUsername: false })(state)).toEqual(
+        steps.CHECKS_COMPLETE
+      )
+    })
   })
 })
 
@@ -39,115 +266,5 @@ describe('fieldIsActive', () => {
   it('returns false for other combinations', () => {
     expect(fieldIsActive('email', steps.USER_EXISTS_BY_USERNAME)).toEqual(false)
     expect(fieldIsActive('foo', steps.USER_EXISTS_BY_USERNAME)).toEqual(false)
-  })
-})
-
-describe('getFieldAvailability', () => {
-  it('selects fieldAvailability substate', () => {
-    expect(
-      getFieldAvailability(state => state.registration)({
-        registration: {
-          fieldAvailability: 0
-        }
-      })
-    ).toEqual(0)
-  })
-})
-
-describe('getStep', () => {
-  it(`evaluates state for step ${steps.PROVIDE_EMAIL}`, () => {
-    state = {
-      registration: {
-        fieldAvailability: {
-          userExistsByEmail: undefined,
-          emailExists: undefined,
-          verificationCodeSendOk: undefined,
-          verificationCodeMatches: undefined,
-          userExistsByUsername: undefined
-        }
-      }
-    }
-
-    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_EMAIL)
-  })
-
-  it(`evaluates state for step ${steps.PROVIDE_EMAIL}`, () => {
-    state = {
-      registration: {
-        fieldAvailability: {
-          userExistsByEmail: true,
-          emailExists: undefined,
-          verificationCodeSendOk: undefined,
-          verificationCodeMatches: undefined,
-          userExistsByUsername: undefined
-        }
-      }
-    }
-
-    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_EMAIL)
-  })
-
-  it(`evaluates state for step ${steps.PROVIDE_VERIFICATION_CODE}`, () => {
-    state = {
-      registration: {
-        fieldAvailability: {
-          userExistsByEmail: false,
-          emailExists: true,
-          verificationCodeSendOk: true,
-          verificationCodeMatches: undefined,
-          userExistsByUsername: undefined
-        }
-      }
-    }
-
-    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_VERIFICATION_CODE)
-  })
-
-  it(`evaluates state for step ${steps.VERIFICATION_CODE_SEND_ERROR}`, () => {
-    state = {
-      registration: {
-        fieldAvailability: {
-          userExistsByEmail: false,
-          emailExists: true,
-          verificationCodeSendOk: false,
-          verificationCodeMatches: undefined,
-          userExistsByUsername: undefined
-        }
-      }
-    }
-
-    expect(getStep(registrationSelector)(state)).toEqual(steps.VERIFICATION_CODE_SEND_ERROR)
-  })
-
-  it(`evaluates state for step ${steps.PROVIDE_USERNAME}`, () => {
-    state = {
-      registration: {
-        fieldAvailability: {
-          userExistsByEmail: false,
-          emailExists: true,
-          verificationCodeSendOk: true,
-          verificationCodeMatches: true,
-          userExistsByUsername: undefined
-        }
-      }
-    }
-
-    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_USERNAME)
-  })
-
-  it(`evaluates state for step ${steps.PROVIDE_USERNAME}`, () => {
-    state = {
-      registration: {
-        fieldAvailability: {
-          userExistsByEmail: false,
-          emailExists: false,
-          verificationCodeSendOk: undefined,
-          verificationCodeMatches: undefined,
-          userExistsByUsername: undefined
-        }
-      }
-    }
-
-    expect(getStep(registrationSelector)(state)).toEqual(steps.PROVIDE_USERNAME)
   })
 })
